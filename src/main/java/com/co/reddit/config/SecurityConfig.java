efa8529aec112ed0fed5ad973a00ff3728d8a97a
Erick <erickjhorman@gmail.com>
2021-01-16 18:24:26 -0500

    JWT Token system created

--- src/main/java/com/co/reddit/config/SecurityConfig.java
+++ src/main/java/com/co/reddit/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package com.co.reddit.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity //Enable spring security module
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception { //take httpsecurity as input to disable csrf protection for app
@@ -19,9 +28,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                      .anyRequest()
                      .authenticated();
     }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER) //indicate spring whta kind of bean will use AuthenticationManager so in this case is used by WebSecurityConfigurerAdapter
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception { //This method allows to intercept request to get the token
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
 }
--- src/main/java/com/co/reddit/service/AuthService.java
+++ src/main/java/com/co/reddit/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.co.reddit.service;
 
+import com.co.reddit.dto.AuthenticationResponse;
+import com.co.reddit.dto.LoginRequest;
 import com.co.reddit.dto.RegisterRequest;
 import com.co.reddit.exception.SpringRedditException;
 import com.co.reddit.model.NotificationEmail;
@@ -7,8 +9,13 @@ import com.co.reddit.model.User;
 import com.co.reddit.model.VerificationToken;
 import com.co.reddit.repository.UserRepository;
 import com.co.reddit.repository.VerificationTokenRepository;
+import com.co.reddit.security.JwtProvider;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +33,8 @@ public class AuthService {
     private final UserRepository userRepository;
     private final VerificationTokenRepository verificationTokenRepository;
     private final MailService mailService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
 
     @Transactional
     public void signup(RegisterRequest registerRequest){
@@ -66,5 +75,16 @@ public class AuthService {
         User user = userRepository.findByUsername(username).orElseThrow(() -> new SpringRedditException("User not found with name - " + username));
         user.setEnabled(true);
         userRepository.save(user);
+    }
+
+    public AuthenticationResponse login(LoginRequest loginRequest) {
+         Authentication authenticate =  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername()
+                      ,loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authenticate);  /* SecurityContext is used to store the details of the currently authenticated user,So, if you have to get the
+                                                                            username or any other user details, you need to get this SecurityContext first.*/
+
+        String token = jwtProvider.generateToken(authenticate);
+        return new AuthenticationResponse(token,loginRequest.getUsername());
+
     }
 }
